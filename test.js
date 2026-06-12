5c58c978b12e9e82e2320a787dacf121a01c3086
sahaj srivastava <sahajsrivastava044@gmail.com>
2026-06-12 03:38:54 +0000

    Changed the test.js file

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-const fs = require("fs");
-
-console.log("Running basic tests...");
-
-if (!fs.existsSync("server.js")) {
-  console.error("server.js does not exist");
-  process.exit(1);
-}
-
-console.log("All tests passed");
-process.exit(0);
+console.log("Running broken test...");
+console.error("Test failed intentionally");
+process.exit(1);
