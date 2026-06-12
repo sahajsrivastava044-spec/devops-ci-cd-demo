3561d4866d6cb577c3dbf3f93ea1e62e40929d18
sahaj srivastava <sahajsrivastava044@gmail.com>
2026-06-12 03:44:20 +0000

    Fixed the test file

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-console.log("Running broken test...");
-console.error("Test failed intentionally");
-process.exit(1);
+const fs = require("fs");
+
+console.log("Running basic tests...");
+
+if (!fs.existsSync("server.js")) {
+  console.error("server.js does not exist");
+  process.exit(1);
+}
+
+console.log("All tests passed");
+process.exit(0);
